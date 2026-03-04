e1d2d33a25f8ce3343b2c3e5fb788275f8d1a672
Huynh Phuc Loi <huynhphucloi007@gmail.com>
2026-03-04 12:17:37 +0700

    thêm hiệu ứng click bong bóng

--- click.js
+++ click.js
@@ -1,6 +1,27 @@
-$(".full-landing-image").ripples({
-	resolution: 256,
-	perturbance: 0.01,
-});
+document.addEventListener("click", function(e){
 
-$('.full-landing-image').ripples('drop', 500, 300, 20, 0.04);
+    for(let i = 0; i < 20; i++){
+
+        const bubble = document.createElement("span");
+        bubble.className = "bubble";
+
+        bubble.style.left = e.clientX + "px";
+        bubble.style.top = e.clientY + "px";
+
+        const size = Math.random()*12 + 6;
+        bubble.style.width = size + "px";
+        bubble.style.height = size + "px";
+
+        const x = (Math.random()-0.5)*120;
+        const y = Math.random()*120;
+
+        bubble.style.transform = `translate(${x}px,-${y}px)`;
+
+        document.body.appendChild(bubble);
+
+        setTimeout(()=>{
+            bubble.remove();
+        },1200);
+    }
+
+});
